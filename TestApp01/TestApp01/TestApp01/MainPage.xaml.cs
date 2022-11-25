e9eb83e68ac1b64aa6f03b46255556670018c720
HenryAndrew356 <58161132+HenryAndrew356@users.noreply.github.com>
2022-11-25 18:32:35 -0500

    first commit

---
+++ TestApp01/TestApp01/TestApp01/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestApp01
+{
+    public partial class MainPage : ContentPage
+    {
+        // El public tiene que tener el mismo nombre
+        public MainPage()
+        {
+            //Pagina de Inicilizacion
+            InitializeComponent();
+                //Especificando la pagina de arranque
+            //Main Page=new MainPage();
+        }
+    }
+}

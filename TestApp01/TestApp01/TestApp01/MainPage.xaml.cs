7b343b4e977e5fd839a9f06d00943628dfa40433
HenryAndrew356 <58161132+HenryAndrew356@users.noreply.github.com>
2022-11-26 22:01:07 -0500

    add changes

--- TestApp01/TestApp01/TestApp01/MainPage.xaml.cs
+++ TestApp01/TestApp01/TestApp01/MainPage.xaml.cs
@@ -6,17 +6,21 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace TestApp01
-{
-    public partial class MainPage : ContentPage
-    {
+namespace TestApp01{
+    public partial class MainPage : ContentPage{
         // El public tiene que tener el mismo nombre
-        public MainPage()
-        {
+        public MainPage(){
             //Pagina de Inicilizacion
             InitializeComponent();
                 //Especificando la pagina de arranque
             //Main Page=new MainPage();
         }
+        private void Editor_TextChanged(object sender, TextChangedEventArgs e){
+            string pasado = e.OldTextValue;
+            string nuevo = e.NewTextValue;
+        }
+        private void Editor_Completed(object sender, TextChangedEventArgs e){
+            string textFinal = ((Editor)sender).Text;
+        }
     }
-}
+}   
